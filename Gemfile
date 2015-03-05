54efe447278a890717812bc5902356cc65d115cc
Stephen Lloyd <stephen@Stephens-MacBook-Pro-2.local>
2015-03-05 12:56:21 +0000

    added json p

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://www.rubygems.org'
 gem 'sinatra'
+gem 'sinatra-jsonp'
 
 gem 'rspec'
 gem 'capybara'
--- server.rb
+++ server.rb
@@ -1,14 +1,14 @@
 require 'sinatra/base'
 require 'json'
 require 'yaml'
-
+require 'sinatra/jsonp'
 class Temperature < Sinatra::Base
-
+  helpers Sinatra::Jsonp
   CITY_DATA = YAML.load_file('./data/city_data.yml')
 
   get '/temperature' do
     return {error: "no city given"}.to_json unless params[:city]
-    city_data(params[:city].gsub(" ", '_').downcase).to_json
+    jsonp(city_data(params[:city].gsub(" ", '_').downcase))
   end
 
   def city_data(city)
@@ -16,11 +16,12 @@ class Temperature < Sinatra::Base
   end
 
   get '/cities' do
-    {cities: CITY_DATA.keys.map{|city|city.gsub("_"," ")}}.to_json
+    jsonp({cities: CITY_DATA.keys.map{|city|city.gsub("_"," ")}})
   end
 
   get '/' do
     "to get a list of the available cities go to /cities \n to get the temperature in a chosen city go to /temperature?city=city_name"
   end
+
 
 end
